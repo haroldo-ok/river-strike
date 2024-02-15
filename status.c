3e399fe3d1259ae7381f55f0c16215c5caa2787c
Haroldo de Oliveira Pinheiro <haroldo@atsinformatica.com.br>
2024-02-15 20:01:32 -0300

    Turn fuel gauge into energy bar.

--- status.c
+++ status.c
@@ -3,15 +3,12 @@
 #include "status.h"
 
 #define FUEL_TILE_LEFT (22)
-#define FUEL_TILE_NEEDLE (24)
-#define FUEL_TILE_RIGHT (26)
 
-#define FUEL_TOP (SCREEN_H - 18)
-#define FUEL_LEFT (128 - 32 + 8)
-#define FUEL_RIGHT (128 + 32)
+#define FUEL_TOP (24)
+#define FUEL_LEFT (16)
 
 void draw_fuel_gauge() {
-	SMS_addSprite(FUEL_LEFT, FUEL_TOP, FUEL_TILE_LEFT);
-	SMS_addSprite(FUEL_RIGHT, FUEL_TOP, FUEL_TILE_RIGHT);	
-	SMS_addSprite(FUEL_LEFT + 16, FUEL_TOP, FUEL_TILE_NEEDLE);
+	for (char count = 4, y = FUEL_TOP, tile = FUEL_TILE_LEFT; count; count--, y += 16, tile += 2) {
+		SMS_addSprite(FUEL_LEFT, y, tile);
+	}
 }
